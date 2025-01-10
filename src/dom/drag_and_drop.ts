34795337f20e45268ef7f15bb8ff940dcaeeea02
Facundo Javier Gelatti <javiergelatti@gmail.com>
2025-01-09 22:20:00 -0300

    Eliminar código no usado

--- src/dom/drag_and_drop.ts
+++ src/dom/drag_and_drop.ts
@@ -34,37 +34,6 @@ export function makeDraggable(
     });
 }
 
-export function makeDropTarget(element: HTMLElement, onDrop: (droppedElement: HTMLElement) => void) {
-    const abortController = new AbortController();
-
-    element.addEventListener("dragenter", (e) => {
-        element.classList.add("drop-target");
-        e.preventDefault();
-    }, { signal: abortController.signal });
-    element.addEventListener("dragover", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-    }, { signal: abortController.signal });
-    element.addEventListener("dragleave", (e) => {
-        element.classList.remove("drop-target");
-        e.preventDefault();
-    }, { signal: abortController.signal });
-    element.addEventListener("drop", (e) => {
-        element.classList.remove("drop-target");
-        const sourceElementId = e.dataTransfer!.getData("text/plain");
-        const sourceElement = document.getElementById(sourceElementId)!;
-
-        onDrop(sourceElement);
-
-        e.stopPropagation();
-        e.preventDefault();
-    }, { signal: abortController.signal });
-
-    return () => {
-        abortController.abort();
-    }
-}
-
 export function makeDropTargetExpecting(
     dropTargetElement: HTMLElement,
     expectedDropElement: HTMLElement,
