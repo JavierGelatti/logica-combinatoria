d96328aa541cfe1366ec6f7abf5d2eecd97626f4
Facundo Javier Gelatti <javiergelatti@gmail.com>
2025-01-10 00:17:50 -0300

    Arreglar drag & drop para que funcione en Chrome para Android

--- src/dom/drag_and_drop.ts
+++ src/dom/drag_and_drop.ts
@@ -8,7 +8,7 @@ export type DraggableConfiguration = {
     dropEffect?: DropEffect,
 };
 
-function randomId() {
+function randomUUID() {
     if (!crypto.randomUUID) {
         return `${randomDigits(8)}-${randomDigits(4)}-${randomDigits(4)}-${randomDigits(4)}-${randomDigits(12)}` as const;
 
@@ -20,18 +20,26 @@ function randomId() {
     return crypto.randomUUID();
 }
 
+function isUUID(text: string) {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+    return uuidRegex.test(text);
+}
+
 export function makeDraggable(
     element: HTMLElement,
     configuration: DraggableConfiguration = {}
 ) {
     const { onDragStart, onDrop, onDragEnd, dropEffect } = Object.assign({ dropEffect: "copy" }, configuration);
 
-    element.id = randomId();
+    element.id = randomUUID();
     element.setAttribute("draggable", "true");
     element.addEventListener("dragstart", (e) => {
         if (e.dataTransfer === null) return;
 
+        // See comment [657fabfb-7fbe-46ab-805e-cc1a74517ff4]
         e.dataTransfer.setData(element.id, "");
+        e.dataTransfer.setData("text/plain", element.id);
+
         e.dataTransfer.dropEffect = dropEffect;
         onDragStart?.();
         e.stopPropagation();
@@ -49,7 +57,8 @@ export function makeDraggable(
 export function makeDropTargetExpecting(
     dropTargetElement: HTMLElement,
     expectedDropElement: HTMLElement,
-    onDrop: () => void
+    onDrop: () => void,
+    dropEffect: DropEffect = "copy",
 ) {
     const abortController = new AbortController();
     const expectedElementId = expectedDropElement.id;
@@ -57,11 +66,13 @@ export function makeDropTargetExpecting(
     const enabledDropTargetClass = `drop-target-enabled-for-${expectedElementId}`;
     dropTargetElement.classList.add(enabledDropTargetClass);
 
-    addEventListenerToTarget("dragenter", () => {
+    addEventListenerToTarget("dragenter", e => {
         dropTargetElement.classList.add("drop-target");
+        if (e.dataTransfer) e.dataTransfer.dropEffect = dropEffect;
     });
-    addEventListenerToTarget("dragover", (e) => {
+    addEventListenerToTarget("dragover", e => {
         dropTargetElement.classList.add("drop-target");
+        if (e.dataTransfer) e.dataTransfer.dropEffect = dropEffect;
         e.stopPropagation();
     });
     addEventListenerToTarget("dragleave", () => {
@@ -83,14 +94,34 @@ export function makeDropTargetExpecting(
         dropTargetElement.addEventListener(
             type,
             function(event) {
-                const sourceElementId = event.dataTransfer?.types[0];
-                if (sourceElementId === expectedElementId) {
+                if (event.dataTransfer === null) return;
+
+                if (isForExpectedTarget(event.dataTransfer)) {
                     listener.bind(this)(event);
                     event.preventDefault();
                 }
             },
             { signal: abortController.signal }
         );
+    }
+
+    function isForExpectedTarget(dataTransfer: DataTransfer) {
+        if (dataTransfer.types[0] === expectedElementId) return true;
+
+        // [657fabfb-7fbe-46ab-805e-cc1a74517ff4]
+        // The verification is weird because some browsers (e.g. Chrome) only allow to access the data of the
+        // dataTransfer object on _drop_. For those cases, we store the data as the _type_, so we can access it
+        // before the drop. Other browsers (e.g. Chrome for Android) do not like a custom type: they only support, for
+        // instance, text/plain.
+        // As we want the drag & drop feature to work on the worst case, we skip the check and allow the event if we
+        // can't get any information about its source.
+        const sourceElementIdFromType = dataTransfer.types.find(type => isUUID(type));
+        const sourceElementIdFromData = dataTransfer.getData("text/plain");
+
+        const didNotGetAnyInfo = sourceElementIdFromType === undefined && sourceElementIdFromData === "";
+        if (didNotGetAnyInfo) return true;
+
+        return sourceElementIdFromType === expectedElementId || sourceElementIdFromData === expectedElementId;
     }
 
     function endInteraction() {
